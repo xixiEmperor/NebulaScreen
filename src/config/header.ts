f360606717e9a74beb28cb7f4033c53ea45c9168
喜玺 <2559762795@qq.com>
2025-09-24 01:05:39 +0800

    feat(index): 首屏以及头部组件开发基本完成

---
+++ src/config/header.ts
@@ -0,0 +1,12 @@
+import type { NavItem } from "@/types/header";
+
+export const NavItems: NavItem[] = [
+  {
+    label: "我的项目",
+    href: "/projects",
+  },
+  {
+    label: "组件库",
+    href: "/components",
+  },
+]
